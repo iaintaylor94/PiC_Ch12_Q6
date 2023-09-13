aefa51d09f8006fa3b5f47232bbf185ace395443
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-13 13:22:08 +0000

    completed program

--- .ccls-cache/@home@runner@Ch12Q6-bit-pattern-search/main.c
+++ .ccls-cache/@home@runner@Ch12Q6-bit-pattern-search/main.c
@@ -1,6 +1,46 @@
+// Program to find a bit pattern within another unsigned int and to return the starting bit if it exists
+
 #include <stdio.h>
 
+// Declare Functions
+int int_size (void);
+int bitpat_search (unsigned int, unsigned int, int);
+
 int main(void) {
-  printf("Hello World\n");
+
+  unsigned int pat = 13; // 4 bits // 1101
+  unsigned int in = 13 << 4;
+  int num = 5;
+
+  printf ("       1111222222222233\n");
+  printf ("       6789012345678901\n");
+  printf ("in:  %#.16b\n", in);
+  printf ("pat: %#.16b\n", pat);
+  
+  printf ("Patern starts at bit: %d\n", bitpat_search(in, pat, num));
+  
   return 0;
+}
+
+// Define Functions
+int int_size (void) {
+  unsigned int test = ~0; // All ones
+
+  int i = 0;
+  while (test != 0) {
+    test >>= 1;
+    i++;
+  }
+
+  return i;
+}
+
+// Starting from the leftmost bits "0" -> "int_size - numBits"
+int bitpat_search (unsigned int source, unsigned int pattern, int numBits) {
+  pattern <<= int_size() - numBits;
+  for (int i = 0; i < int_size() - numBits; i++, pattern >>= 1) {
+    if ((source & pattern) == pattern)
+      return i;
+  }
+  return -1;
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Programming in C
+## Chapter 12
+### Question 6
+
+This problem is from chapter 12 of the textbook[^1]. This program searched for a specific bit pattern within another unsigned int and returns the starting bit if it exists.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 297 : Question 6
